e6ed603415a7bf1606458ef97371dde26197bc90
chaimaeddib2005 <Chaima.EDDIB@um6p.ma>
2025-04-30 10:48:40 +0000

    Ok

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,25 @@
       <router-view></router-view>
     </div>
 </template>
+<script>
+export default {
+  name: 'App',
+  
+}
+</script>
 
 
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+#app{
+  margin:0;
+}
+</style>
